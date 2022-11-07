313679d51050cf45e312b34356896620955f8927
meluvrose <tanggu3910@gmail.com>
2022-11-07 23:31:45 +0900

    #4.1 Migrations

--- houses/models.py
+++ houses/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class House(models.Model):
 
     """Model definitions for Houses"""
-
+    
     name = models.CharField(max_length=140)
     price = models.PositiveIntegerField()
     description = models.TextField()
