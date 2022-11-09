ea35f10c6306e434e5b8db1e7c1adcb00469a5ba
meluvrose <tanggu3910@gmail.com>
2022-11-09 16:51:04 +0900

    #6.0 User Model

--- users/models.py
+++ users/models.py
@@ -3,7 +3,43 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150, editable=False)
-    last_name = models.CharField(max_length=150, editable=False)
-    name = models.CharField(max_length=150, default="")
+    class GenderChoices(models.TextChoices):
+        # this tuple items are value in DB
+        # and label for page of 'admin'
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrencyChoices(models.TextChoices):
+        WON = "won", "Korean won"
+        WSD = "usd", "Dollar"
+
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
     is_host = models.BooleanField(default=False)
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoices.choices,
+    )
+    curreny = models.CharField(
+        max_length=5,
+        choices=CurrencyChoices.choices,
+    )
