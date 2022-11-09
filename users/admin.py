b5cfd4a998ab6969d53ae76b0a1523c046c27b0b
meluvrose <tanggu3910@gmail.com>
2022-11-09 15:33:16 +0900

    #5.5 Foreign Keys

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,35 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (
+            # "Profile",
+            None,
+            {
+                "fields": ("username", "password", "name", "email", "is_host"),
+                "classes": ("wide",),
+            },
+        ),
+        (
+            "permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    list_displayb = ("username", "email", "name", "is_host")
