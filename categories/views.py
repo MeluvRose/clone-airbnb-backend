d82c54528496035eb046b52754b3d2503df23b8e
meluvrose <tanggu3910@gmail.com>
2022-11-25 12:25:01 +0900

    #10.2 api_view

--- categories/views.py
+++ categories/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
-from .models import Category
 from django.http import JsonResponse
+from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Category
 
 
+@api_view()
 def categories(request):
-    all_categories = Category.objects.all()
-    return JsonResponse(
+    # all_categories = Category.objects.all()
+    # return JsonResponse(
+    #     {
+    #         "ok": True,
+    #         "categories": serializers.serialize(
+    #             "json",
+    #             all_categories,
+    #         ),
+    #     }
+    # )
+
+    return Response(
         {
             "ok": True,
-            "categories": all_categories,
         }
     )
